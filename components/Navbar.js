1910c9b0fdfc0d81acf175cc794d3e4b5a726aac
Pecista - Distribuição e Representação de Autopeças <tecnologia.pecista@gmail.com>
2022-03-22 16:36:08 -0300

    Commit Navbar "Loja online"

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,8 @@ import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap'
 import Image from 'next/image'
 import Link from 'next/link'
 import { AiOutlineWhatsApp } from 'react-icons/ai'
+import { AiFillInstagram } from 'react-icons/ai'
+import { AiFillShop } from 'react-icons/ai'
 
 
 const NavBar = () => {
@@ -23,19 +25,28 @@ const NavBar = () => {
               <Link href="/unidades"><a className="nav-link">Unidades</a></Link>
               <Link href="/fornecedores"><a className="nav-link">Fornecedores</a></Link>
               <Link href="/sac"><a className="nav-link">SAC</a></Link>
+              <Link href="https://kaizenautopecas.mercadoshops.com.br/"><a className="nav-link" target='_blank'>Loja Online</a></Link>
               <NavDropdown title="Catálogos" id="basic-nav-dropdown" style={{ color: '#fff' }}>
                 <NavDropdown.Item >Elétrica</NavDropdown.Item>
                 <NavDropdown.Item >Óleos</NavDropdown.Item>
                 <NavDropdown.Item >Mecânica</NavDropdown.Item>
                 <NavDropdown.Divider />
-                <a href="https://drive.google.com/file/d/1s3f_HNw0pK2J5SNq-fayciRl-WzN4n3w/view" target='_blank' style={{marginLeft: "8px"}}>Catálogo Completo</a>
-              </NavDropdown>           
-            </Nav>
-            <a href="https://api.whatsapp.com/send/?phone=556183660828&text=Ol%C3%A1%2C+eu+gostaria+de+um+or%C3%A7amento.&app_absent=0" target='_blank' className="btn ml-auto" style={{ borderRadius: "2px" }}>
-                <button type="button" className="btn btn-primary my-3"><AiOutlineWhatsApp className="m-1" /> SOLICITAR ORÇAMENTO</button>
-              </a >
+                <a href="https://drive.google.com/file/d/1s3f_HNw0pK2J5SNq-fayciRl-WzN4n3w/view" target='_blank' style={{marginLeft: "8px"}}>Catálogo Completo</a>                
+              </NavDropdown>
+              
+              </Nav>
+              
+            
+            
+          
           </Navbar.Collapse>
-        </div>
+          <a href="https://www.instagram.com/casa.kaizen/" target='_blank' style={{ borderRadius: "2px" }}><AiFillInstagram className="m-1" style={{ width: "50px", height: "40px", }}/></a>
+              {/* <a href="https://kaizenautopecas.mercadoshops.com.br/" target='_blank' style={{ borderRadius: "2px" }}><AiFillShop className="m-1" style={{ width: "50px", height: "40px", }}/></a> */}
+              <a href="https://api.whatsapp.com/send/?phone=556183660828&text=Ol%C3%A1%2C+eu+gostaria+de+um+or%C3%A7amento.&app_absent=0" target='_blank' className="btn ml-auto" style={{ borderRadius: "2px" }}>
+                <button type="button" className="btn btn-primary my-3"><AiOutlineWhatsApp className="m-1" />SOLICITAR ORÇAMENTO</button>
+             
+              </a >
+        </div>        
       </Navbar>
     </>
   )
